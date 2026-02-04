0988ce46719fc93ef36739129400771ab092709f
nadilk <karimov.nadil.96@gmail.com>
2026-02-05 00:04:23 +0500

    problem6 -  insertionSort

--- index.js
+++ index.js
@@ -4,9 +4,11 @@ import problem2 from './problems/problem2.js';
 import problem3 from './problems/problem3.js';
 import problem4 from './problems/problem4.js';
 import problem5 from "./problems/problem5.js";
+import problem6 from "./problems/problem6.js";
 
 //console.log(problem1(2,3));
 //console.log(problem2('abcddba'));
 //console.log(problem3('aaabccddabab'));
 //console.log(problem4('aaabccddababbb', 3));
-console.log(problem5([2, 1, 4, 5, 3]));
+//console.log(problem5([2, 1, 4, 5, 3]));
+console.log(problem6([20, 1, 41, 5, 300]));
