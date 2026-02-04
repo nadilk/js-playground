f82622bad9f92f91cf1a904c16f9af2e22710443
nadilk <karimov.nadil.96@gmail.com>
2026-02-04 23:21:42 +0500

    problem5 - bubble sort

--- index.js
+++ index.js
@@ -3,8 +3,10 @@ import problem1 from './problems/problem1.js';
 import problem2 from './problems/problem2.js';
 import problem3 from './problems/problem3.js';
 import problem4 from './problems/problem4.js';
+import problem5 from "./problems/problem5.js";
 
 //console.log(problem1(2,3));
 //console.log(problem2('abcddba'));
 //console.log(problem3('aaabccddabab'));
 //console.log(problem4('aaabccddababbb', 3));
+console.log(problem5([2, 1, 4, 5, 3]));
